d913a7efbb2539407b1d911615c9751685a5a42e
Fearless09 <toheebopeyemi9@gmail.com>
2025-11-11 18:56:09 +0100

    Homepage Responsiveness

--- src/components/shared/Companies.tsx
+++ src/components/shared/Companies.tsx
@@ -3,23 +3,28 @@ import Image from "next/image";
 const Companies = () => {
   return (
     <section className="relative py-5">
-      <section className="wrapper overflow-hidden px-4">
-        <main className="flex flex-nowrap items-center gap-6">
-          {companies.map((company, index) => (
-            <div
-              key={index}
-              className="relative aspect-[2.74/1.01] min-w-[179px] flex-1 shrink-0 overflow-clip"
-            >
-              <Image
-                alt={company}
-                src={company}
-                fill
-                className="object-contain object-center"
-                sizes="100%"
-              />
-            </div>
-          ))}
-        </main>
+      <section className="wrapper flex flex-nowrap items-center overflow-hidden px-4">
+        {[...Array(5)].map((_, arrIdx) => (
+          <main
+            key={arrIdx}
+            className="animate-marquee flex w-full flex-nowrap items-center gap-5 pl-5"
+          >
+            {companies.map((company, index) => (
+              <div
+                key={index}
+                className="relative aspect-[2.74/1.01] min-w-[179px] flex-1 shrink-0 overflow-clip"
+              >
+                <Image
+                  alt={company}
+                  src={company}
+                  fill
+                  className="object-contain object-center"
+                  sizes="100%"
+                />
+              </div>
+            ))}
+          </main>
+        ))}
       </section>
     </section>
   );
